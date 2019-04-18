fd849c12bd695221f33e5f05cf25506438eaaa68
mydongistiny <jaysonedson@gmail.com>
2019-04-18 14:14:27 -0700

    art: Remove defined product speed apps
    Let these be defined per device
    
    Change-Id: Idf8a033abb2a72befc8e9d26372009f42125b0d7
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- config/art.mk
+++ config/art.mk
@@ -9,19 +9,9 @@ ART_BUILD_HOST_DEBUG := false
 WITH_DEXPREOPT := true
 WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
 
-# Preopt apks like GMS chimara modules
-PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
-
 # Create profile for boot image
 # Outputs to /system/etc/boot-image.prof
 PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
-
-# Speed apps
-PRODUCT_DEXPREOPT_SPEED_APPS += \
-  GoogleCamera \
-  NexusLauncherPrebuilt \
-  SystemUI \
-  Settings
 
 # Dex pre-opt profiles
 WITH_DEX_PREOPT_GENERATE_PROFILE := true
