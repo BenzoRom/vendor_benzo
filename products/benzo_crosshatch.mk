3ef7cf53afe9cee3a72393f015f748f454041ede
mydongistiny <jaysonedson@gmail.com>
2019-08-05 17:11:51 -0700

    products: Update crosshatch build desc for PQ3A.190801.002
    Change-Id: Idfb74c1447b6e672316f81554fb4d8a4d3170994
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- products/benzo_crosshatch.mk
+++ products/benzo_crosshatch.mk
@@ -20,10 +20,10 @@ PRODUCT_MANUFACTURER := Google
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=crosshatch \
-    PRIVATE_BUILD_DESC="crosshatch-user 9 PQ3A.190705.003 5600800 release-keys"
+    PRIVATE_BUILD_DESC="crosshatch-user 9 PQ3A.190801.002 5670241 release-keys"
 
-BUILD_FINGERPRINT="google/crosshatch/crosshatch:9/PQ3A.190705.003/5600800:user/release-keys"
-BUILD_THUMBPRINT="9/PQ3A.190705.003/5600800:user/release-keys"
+BUILD_FINGERPRINT="google/crosshatch/crosshatch:9/PQ3A.190801.002/5670241:user/release-keys"
+BUILD_THUMBPRINT="9/PQ3A.190801.002/5670241:user/release-keys"
 
 # Copy device specific prebuilt files.
 PRODUCT_COPY_FILES += \
