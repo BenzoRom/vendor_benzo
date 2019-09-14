fc62c6d0054dbd5ef84d17236354c66787d03e84
Jason Edson <jaysonedson@gmail.com>
2019-09-14 14:19:22 -0700

    prebuilt: Add fonts customization from Pixel
    Add fonts_customization.xml, Google Sans ttf and build
    the required font packages.
    
    Change-Id: I76b2c0e14f34d6d2883ffbcea3e5d960a519bcce
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -7,6 +7,9 @@ $(call inherit-product, vendor/benzo/config/versions.mk)
 
 PRODUCT_PACKAGE_OVERLAYS += vendor/benzo/overlay
 
+PRODUCT_PACKAGES += \
+    fonts_customization.xml
+
 # Enable tethering
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.sys.dun.override=0
