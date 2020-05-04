4762ffd9d0cff1276dbe104670080b83f8bba2c3
mydongistiny <jaysonedson@gmail.com>
2020-05-04 11:38:58 -0700

    products: Update crosshatch build desc for QQ2A.200501.001.B2
    Change-Id: I2b860308ec4c986e1ace84d50b75c541fc290d6e
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- products/benzo_crosshatch.mk
+++ products/benzo_crosshatch.mk
@@ -35,7 +35,7 @@ PRODUCT_DEVICE := crosshatch
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=crosshatch \
-    PRIVATE_BUILD_DESC="crosshatch-user 10 QQ2A.200405.005 6254899 release-keys"
+    PRIVATE_BUILD_DESC="crosshatch-user 10 QQ2A.200501.001.B2 6352890 release-keys"
 
-BUILD_FINGERPRINT="google/crosshatch/crosshatch:10/QQ2A.200405.005/6254899:user/release-keys"
-BUILD_THUMBPRINT="10/QQ2A.200405.005/6254899:user/release-keys"
+BUILD_FINGERPRINT="google/crosshatch/crosshatch:10/QQ2A.200501.001.B2/6352890:user/release-keys"
+BUILD_THUMBPRINT="10/QQ2A.200501.001.B2/6352890:user/release-keys"
