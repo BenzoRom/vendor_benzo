b0ddf0bdd35e1af8d8b4bc0c55e43b27e675408a
Jason Edson <jaysonedson@gmail.com>
2021-12-05 21:22:44 -0800

    benzo: build the Bourne Again SHell
    And add system artifacts to allowed list.
    
    Change-Id: Ib7fdc697d3745ec4e5204f3eca60490651089f7c
    Signed-off-by: Jason Edson <jaysonedson@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -16,6 +16,16 @@ PRODUCT_PACKAGES += \
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     persist.sys.dun.override=0
 
+# Bash
+PRODUCT_PACKAGES += \
+    bash
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/bin/bash \
+    system/etc/bash/bash_logout \
+    system/etc/bash/bashrc \
+    system/etc/bash/inputrc \
+    system/lib64/libncurses.so
+
 # Use ccache
 USE_CCACHE := false
 ifeq ($(filter-out true,$(USE_CCACHE)),)
